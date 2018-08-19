8c7cb7b97f9dc9e41dc5bb893855a6c49dd99cf5
razordead <razordead@gmail.com>
2018-08-19 15:08:23 -0700

    incremented version to 0.3a

--- RazorChat/Properties/Settings.Designer.cs
+++ RazorChat/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RazorChat.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -217,7 +217,7 @@ namespace RazorChat.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v0.2a")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v0.3a")]
         public string AboutVersion {
             get {
                 return ((string)(this["AboutVersion"]));
